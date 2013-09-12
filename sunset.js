491a9d024a6fcabd4d19cc1235af815a230ab887
Ned Gulley <gulley@gmail.com>
2013-09-11 22:02:09 -0400

    Earliest sunset function
    Added a function for determining the day of the earliest sunset

--- sunset.js
+++ sunset.js
@@ -1,7 +1,7 @@
 function decToHms(tIn) {
   var hours = Math.floor(tIn);
-  var minutes = Math.floor((tIn - hours)*60);
-  var seconds = Math.floor((tIn - hours - minutes/60)*60*60);
+  var minutes = Math.floor((tIn - hours) * 60);
+  var seconds = Math.floor((tIn - hours - minutes / 60) * 60 * 60);
   if (hours > 12) {
     hours -= 12;
   }
@@ -9,8 +9,8 @@ function decToHms(tIn) {
 }
 
 function decToMs(tIn) {
-  var minutes = Math.floor(tIn*60);
-  var seconds = Math.floor((tIn - minutes/60)*60*60);
+  var minutes = Math.floor(tIn * 60);
+  var seconds = Math.floor((tIn - minutes / 60) * 60 * 60);
   return minutes + ":" + pad(seconds);
 }
 
@@ -18,70 +18,91 @@ function pad(n) {
   return (n < 10) ? ("0" + n) : n;
 }
 
-function drawHand(ctx,angle,color,length,width) {
+function drawHand(ctx, angle, color, length, width) {
   var mid = 200;
   ctx.beginPath();
   ctx.moveTo(mid, mid);
-  ctx.lineTo(mid+length*Math.cos(angle), mid+length*Math.sin(angle));
+  ctx.lineTo(mid + length * Math.cos(angle), mid + length * Math.sin(angle));
   ctx.lineWidth = width;
   ctx.strokeStyle = color;
   ctx.stroke();
 }
 
-function drawMinuteHand(ctx,tIn,color,width) {
+function drawMinuteHand(ctx, tIn, color, width) {
   var hours = Math.floor(tIn);
-  var minutes = Math.floor((tIn - hours)*60);
-  var angle = 2*Math.PI*(tIn - hours);
+  var minutes = Math.floor((tIn - hours) * 60);
+  var angle = 2 * Math.PI * (tIn - hours);
   // Correct for the fact that east corresponds to 0 degrees, whereas north is 12:00
-  angle -= Math.PI/2;
+  angle -= Math.PI / 2;
   var length = 180;
-  drawHand(ctx,angle,color,length,width);
+  drawHand(ctx, angle, color, length, width);
 }
 
-function drawHourHand(ctx,tIn) {
+function drawHourHand(ctx, tIn) {
   var hours = Math.floor(tIn);
-  var minutes = Math.floor((tIn - hours)*60);
-  var angle = 2*Math.PI*((hours + minutes/60.0)/12.0);
+  var minutes = Math.floor((tIn - hours) * 60);
+  var angle = 2 * Math.PI * ((hours + minutes / 60.0) / 12.0);
   // Correct for the fact that east corresponds to 0 degrees, whereas north is 12:00
-  angle -= Math.PI/2;
-  drawHand(ctx,angle,"#000000",100,8);
+  angle -= Math.PI / 2;
+  drawHand(ctx, angle, "#000000", 100, 10);
 }
 
 function drawClockFace(ctx, option) {
   // Draw the circle to make the clock face
-  var mid = ctx.canvas.getAttribute("width")/2;
+  var mid = ctx.canvas.getAttribute("width") / 2;
 
-  if (option==="face") {
+  if (option === "face") {
     ctx.lineWidth = 2;
     ctx.beginPath();
-    ctx.arc(mid, mid, 195, 0, Math.PI*2, true); 
+    ctx.arc(mid, mid, 195, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = "#FFFFFF";
     ctx.fill();
     ctx.stroke();
   }
 
-  if (option==="nose") {
+  if (option === "nose") {
     ctx.beginPath();
-    ctx.arc(mid, mid, 15, 0, Math.PI*2, true); 
+    ctx.arc(mid, mid, 15, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = "#000000";
     ctx.fill();
   }
-  
+
 }
 
-function getSunsetTime(d,lat,lon) {
+function getEarliestSunset(lat, lon) {
+  var dToday = new Date();
+  var monthDayStr = "";
+  // Start at the equinox and walk forward toward the winter solstice
+  if (lat > 0) {
+    monthDayStr = "9/21/";
+  } else {
+    monthDayStr = "3/21/";
+  }
+  var dateStr = monthDayStr + d.getFullYear();
+  dToday = new Date(dateStr);
+  dTomorrow = new Date(dateStr);
+  dTomorrow.setDate(dTomorrow.getDate() + 1);
+  // As long as the sunset tomorrow is earlier than today, keep going forward in time
+  while (getSunsetTime(dToday, lat, lon) > getSunsetTime(dTomorrow, lat, lon)) {
+    dTomorrow.setDate(dTomorrow.getDate() + 1);
+    dToday.setDate(dToday.getDate() + 1);
+  }
+  return dToday;
+}
+
+function getSunsetTime(d, lat, lon) {
   var sunsetObj = new SunriseSunset(
-    d.getFullYear(), 
-    d.getMonth()+1, 
-    d.getDate(), 
+    d.getFullYear(),
+    d.getMonth() + 1,
+    d.getDate(),
     lat, lon);
-  var tzCorrection = -1*d.getTimezoneOffset()/60;
-  return sunsetObj.sunsetLocalHours(tzCorrection); 
+  var tzCorrection = -1 * d.getTimezoneOffset() / 60;
+  return sunsetObj.sunsetLocalHours(tzCorrection);
 }
 
-function decToHex(val,numDigits) {
+function decToHex(val, numDigits) {
   var str = val.toString(16);
   while (str.length < numDigits) {
     str = "0" + str;
@@ -93,39 +114,47 @@ function showTime() {
   var lat = 42.363339;
   var lon = -71.192136;
   var td = new Date();
- 
+
   var ctx = $('#canvas')[0].getContext("2d");
-  drawClockFace(ctx,"face");
- 
+  drawClockFace(ctx, "face");
+
   d = new Date();
   var clr = "";
   var val = 0;
   var max = 14;
   var wid = 1;
-  for (i=0; i<max; i++) {
-    val = Math.floor(255*(i/max));
-    clr = "#" + decToHex(val,2) + decToHex(val,2) + "FF";
-    d.setDate(d.getDate()+1);
-    drawMinuteHand(ctx,getSunsetTime(d,lat,lon),clr,wid);
+  for (i = 0; i < max; i++) {
+    val = Math.floor(255 * (i / max));
+    clr = "#" + decToHex(val, 2) + decToHex(val, 2) + "FF";
+    d.setDate(d.getDate() + 1);
+    drawMinuteHand(ctx, getSunsetTime(d, lat, lon), clr, wid);
   }
 
-  var sunsetToday = getSunsetTime(td,lat,lon) 
-  td.setDate(td.getDate()+1);
-  var sunsetTomorrow = getSunsetTime(td,lat,lon) 
+  var sunsetToday = getSunsetTime(td, lat, lon)
+  td.setDate(td.getDate() + 1);
+  var sunsetTomorrow = getSunsetTime(td, lat, lon)
 
-  drawMinuteHand(ctx,sunsetToday,"#000000",4);
-  drawHourHand(ctx,sunsetToday);
- 
-  drawClockFace(ctx,"nose");
-  
+  drawMinuteHand(ctx, sunsetToday, "#000000", 6);
+  drawHourHand(ctx, sunsetToday);
+
+  drawClockFace(ctx, "nose");
+
+  var earliestSunset = getEarliestSunset(lat, lon);
+
   var diff = sunsetToday - sunsetTomorrow;
-    $(".time").html(
-      "<table>" +
-      "<tr><th colspan='2'>Sunset for " + td.toDateString() + "</th></tr>" +
-      "<tr><td>Today</td><td>" + decToHms(sunsetToday) + "</td></tr>" + 
-      "<tr><td>Tomorrow</td><td>" + decToHms(sunsetTomorrow) + "</td></tr>" + 
-      "<tr><td>Difference</td><td>" + decToMs(diff) + "</td></tr>" +
-      "</table>"
-    );  
+  var daysBetween = Math.floor((earliestSunset - new Date()) / (1000 * 60 * 60 * 24));
+
+  $(".time").html(
+    "<table>" +
+    "<tr><th colspan='2'>Sunset for " + td.toDateString() + "</th></tr>" +
+    "<tr><td>Today</td><td>" + decToHms(sunsetToday) + "</td></tr>" +
+    "<tr><td>Tomorrow</td><td>" + decToHms(sunsetTomorrow) + "</td></tr>" +
+    "<tr><td>Difference</td><td>" + decToMs(diff) + "</td></tr>" +
+    "<tr><th colspan='2'>Earliest Sunset</th></tr>" +
+    "<tr><td>Date</td><td>" + earliestSunset.toDateString() + "</td></tr>" +
+    "<tr><td>Time</td><td>" + decToHms(getSunsetTime(earliestSunset, lat, lon)) + "</td></tr>" +
+    "<tr><td>Days from Now</td><td>" + daysBetween + "</td></tr>" +
+    "</table>"
+  );
 
 }
